ebc60f6bc2ced1b6dbc5770f4083c13b4fd29195
arnab-tech-oss <arnabrofficial@gmail.com>
2024-11-16 15:16:14 +0530

    multiple image add

---
+++ database/migrations/2024_11_16_093049_create_packages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->string('name');
+            $table->decimal('base_price', 10, 2); // Base price of the package
+            $table->decimal('discount_percentage', 5, 2)->default(0); // Discount if any
+            $table->decimal('net_price', 10, 2);
+            $table->timestamps();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
